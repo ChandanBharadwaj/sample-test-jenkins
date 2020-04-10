e79b53734c6b1e241a1ef6ec9e9551f81d3e1318
chandan <chandan@gmail.com>
2020-04-10 18:56:37 +0530

    first commit

---
+++ src/main/java/com/sai/sample/SampleTestJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.sai.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleTestJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleTestJenkinsApplication.class, args);
+	}
+
+}
